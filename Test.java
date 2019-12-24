2b70bd771ba19d35c928abdba34e2bbe8d0ed369
noura <nouradel975@gmial.com>
2019-12-24 16:14:37 +0200

    s

--- Test.java
+++ Test.java
@@ -2,14 +2,8 @@ Class Test{
 	public static void main(String[] args){
 		
 		int x = 2;
-<<<<<<< HEAD
 		x = 3;
 		System.out.println(x);
-=======
-		x = 4;
-		System.out.print("test auto mergeing");
-		x=3;
->>>>>>> 79a9414fa87d800897a167091d0143c88816ca31
 	}
 	
 	public int gtData(){
